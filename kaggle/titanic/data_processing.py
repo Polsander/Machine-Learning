c711ee4a6aefabf4703b4e4e4cd3403ade8c9a4c
Polsander <oerdmann@uwaterloo.ca>
2026-06-13 21:53:24 -0400

    preprocessed data script

---
+++ kaggle/titanic/data_processing.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+
+def process_age(column):
+
+    new_col = []
+    age_values = []
+
+    for i in range(len(column)):
+        if not np.isnan(column[i]):
+            age_values.append(column[i])
+    
+    # Assign average to NaN
+    mean = round(np.mean(age_values))
+    print('mean', mean)
+    
+    for i in range(len(column)):
+        if np.isnan(column[i]):
+            new_col.append(mean)
+        else:
+            new_col.append(column[i])
+    return new_col
+
+
+def one_hot_encode_port(column):
+    S = []
+    Q = []
+    C = []
+
+    for i in range(len(column)):
+        value = column[i]
+
+        match(value):
+
+            case("S"):
+                S.append(1)
+                Q.append(0)
+                C.append(0)
+            case("Q"):
+                S.append(0)
+                Q.append(1)
+                C.append(0)
+            case("C"):
+                S.append(0)
+                Q.append(0)
+                C.append(1)
+            case _:
+                S.append(1)
+                Q.append(0)
+                C.append(0)
+    return S, Q, C
+
+
+
+def male_female(column):
+    # make male 1 and female -1
+    new_col = []
+    for i in range(len(column)):
+        if column[i] == "male":
+            new_col.append(1)
+        else:
+            new_col.append(-1)
+    return new_col
+
+def process_data(pd_data: pd.DataFrame) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """The full process data function which supports being imported/exported"""
+    data_copy: pd.DataFrame = pd_data
+    print("Any NaN values before processing?", data_copy.isna().values.any())
+
+    data_copy = data_copy.drop(columns=["Ticket", "Cabin"])
+    data_copy["Age"] = process_age(data_copy["Age"])
+    data_copy["Sex"] = male_female(data_copy["Sex"])
+    data_copy["S"], data_copy["Q"], data_copy["C"] = one_hot_encode_port(data_copy["Embarked"])
+    data_copy = data_copy.drop(columns=["Embarked"])
+    data_copy["SibSp"] = data_copy["SibSp"].fillna(0)
+    data_copy["Parch"] = data_copy["Parch"].fillna(0)
+
+    print("Any NaN values after processing?", data_copy.isna().values.any())
+
+    Y = data_copy["Survived"].to_numpy()
+    ids = data_copy["PassengerId"].to_numpy()
+    X = data_copy.drop(columns=["Survived", "PassengerId"]).to_numpy()
+
+    return X, Y, ids
+
+
+if __name__ == "__main__":
+
+    train_file_data = pd.read_csv("data/train.csv")
+    X, Y, ids = process_data(train_file_data)
+
+    np.save("matrices/train/X.npy", X)
+    np.save("matrices/train/Y.npy", Y)
+    np.save("matrices/train/ids.npy", ids)
+    print("Data processing complete and matrices saved in folders")
